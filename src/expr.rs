604bb559623738b06e78e2a08e7d91053dd72f89
Tim Holland <th0114nd@gmail.com>
2024-08-11 12:16:45 -0400

    implment Display for expr

--- src/expr.rs
+++ src/expr.rs
@@ -1,6 +1,5 @@
-use std::fmt;
-use std::fmt::Display;
 use crate::token::Token;
+use std::fmt;
 
 // What can we do with an expr?
 enum Expr<'a> {
@@ -10,6 +9,9 @@ enum Expr<'a> {
         right: Box<Expr<'a>>,
     },
     Grouping(Box<Expr<'a>>),
+    // TODO: this should be larger than just tokens, maybe it should include class literals as
+    // well?
+    // can we do less broad than Box<dyn Any>?
     Literal(Token<'a>),
     Unary {
         operator: Token<'a>,
@@ -18,7 +20,40 @@ enum Expr<'a> {
 }
 
 impl<'a> fmt::Display for Expr<'a> {
-    fn fmt(&self, f: mut fmt::Formatter) {
+    fn fmt(&self, f: &mut fmt::Formatter) -> fmt::Result {
+        match &self {
+            Expr::Literal(token) => write!(f, "{}", token.lexeme),
+            Expr::Grouping(gr) => write!(f, "(group {})", gr),
+            Expr::Unary { operator, right } => write!(f, "({} {right})", operator.lexeme),
+            Expr::Binary {
+                left,
+                operator,
+                right,
+            } => write!(f, "({} {left} {right})", operator.lexeme),
+        }
+    }
+}
 
+#[cfg(test)]
+mod tests {
+    use super::Expr::*;
+    use super::*;
+    use crate::token::TokenType;
+
+    #[rstest::rstest]
+    #[case(Literal(Token{token: TokenType::Star, lexeme:"*", line: 1}), "*")]
+    #[case(Binary{
+        left: Box::new(
+            Unary{
+                operator: Token{token: TokenType::Minus, lexeme: "-", line: 1},
+                right: Box::new(Literal(Token{token: TokenType::Number(123.0), lexeme: "123", line:1})),
+            },
+        ),
+        operator: Token{token: TokenType::Star, lexeme: "*", line: 1},
+        right: Box::new(Grouping(Box::new(Literal(Token{token: TokenType::Number(45.67), lexeme: "45.67", line: 1})))),
+    }, "(* (- 123) (group 45.67))")]
+    fn test_display(#[case] expr: Expr, #[case] want: &str) {
+        let got = format!("{}", expr);
+        assert_eq!(got, want);
     }
 }
